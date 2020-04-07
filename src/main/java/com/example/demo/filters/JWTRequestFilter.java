3bf5acf9196b87a1e1278bec4fcc895744b67034
SeanMcelroy97 <seanpetermcelroy@gmail.com>
2020-04-07 19:15:47 +0100

    Validate JWT token in filter

--- src/main/java/com/example/demo/filters/JWTRequestFilter.java
+++ src/main/java/com/example/demo/filters/JWTRequestFilter.java
@@ -8,9 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.example.demo.model.HealthShopUserDetails;
 import com.example.demo.services.HealthShopUserDetailsService;
 import com.example.demo.util.JwtUtil;
 
@@ -34,10 +39,32 @@ public class JWTRequestFilter extends OncePerRequestFilter{
 		String email = null;
 		String jwt = null;
 		
+		//Get User Details
 		if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
 			jwt = authorizationHeader.substring(7);
 			email = jwtUtil.extractUsername(jwt);
 		}
+		
+		
+		//Valuidate USer details 
+		if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			
+			HealthShopUserDetails userDetails = (HealthShopUserDetails) this.userDetailsService.loadUserByUsername(email);
+		
+			//Validate JWT token
+			if (jwtUtil.validateToken(jwt, userDetails)) {
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+               
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+		}
+		
+		//This filter passes to nect filter once finished
+		filterChain.doFilter(request, response);
 	}
 
 	
