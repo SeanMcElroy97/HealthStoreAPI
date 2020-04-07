c17390575712432243c8ff7a78df1353f37d3983
SeanMcelroy97 <seanpetermcelroy@gmail.com>
2020-04-07 18:15:28 +0100

    Added util class with some JWT functionality, Created Authentication Request Object Plus Authentication Response OBJ

--- src/main/java/com/example/demo/api/HomeController.java
+++ src/main/java/com/example/demo/api/HomeController.java
@@ -1,20 +1,37 @@
 package com.example.demo.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
+import com.example.demo.model.HealthShopUserDetails;
+import com.example.demo.model.authentication.AuthenticationRequest;
 import com.example.demo.repositories.CustomerRepo;
 import com.example.demo.services.CustomerService;
+import com.example.demo.services.HealthShopUserDetailsService;
 
 @RestController("/")
 public class HomeController {
 	
 	@Autowired
 	CustomerService mCustomerService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	HealthShopUserDetailsService healthShopUserDetailsService;
+	
+	
+	///APIs
 	
 	@GetMapping
 	public String homeTest() {
@@ -31,6 +48,22 @@ public class HomeController {
 		Customer cust = new Customer("jj@email", "password", "21 whitehouse avenue", "12345678");
 		return mCustomerService.createNewCustomerEntity(cust);
 		 
+	}
+	
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+		
+		try {
+		authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword())
+				);
+		} catch (BadCredentialsException e){
+			throw new Exception("Incorrect email or password", e);
+		}
+		
+		final HealthShopUserDetails userDetails = HealthShopUserDetailsService.
+		return null;
+		
 	}
 
 }
---
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -0,0 +1,66 @@
+package com.example.demo.util;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.model.HealthShopUserDetails;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtUtil {
+
+	//Hardcoded for test
+    private String SECRET_KEY = "secret";
+
+    //Returns username or email 
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    //Returns expiration
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    //Takes in token. Pulls out payload data
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    //whether token is expired or not
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    //Takes in USerDetails OBJ
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    
+    //Called by generate Token. Uses builder pattern to create a JWT token sets details of expired date. currently 10 hours after connected.
+    private String createToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    
+    //is token good 
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
