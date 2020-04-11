67e6d764fbe5fec55e091d9d2faef9d755c4ab42
SeanMcelroy97 <seanpetermcelroy@gmail.com>
2020-04-11 15:26:36 +0100

    Added test controller to test features without security

--- src/main/java/com/example/demo/api/TestController.java
+++ src/main/java/com/example/demo/api/TestController.java
@@ -1,35 +1,31 @@
 package com.example.demo.api;
 
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Customer;
-import com.example.demo.model.Order;
 import com.example.demo.model.Product;
-import com.example.demo.model.authentication.AuthenticationRequest;
-import com.example.demo.services.CustomerService;
 import com.example.demo.services.ProductService;
 
 @RestController
-@RequestMapping("/admin")
-public class AdminController {
-	
+@RequestMapping("/test")
+public class TestController {
+
 	@Autowired
 	ProductService mProductService;
-
-	@GetMapping("")
-	public String test() {
-		return "test admin reached";
+	
+	
+	@GetMapping("/yup")
+	public String yup() {
+		System.out.println("yup reached");
+		return "Danone";
 	}
 	
-	@PostMapping("/createProduct")
+	@PostMapping("/createproduct")
 	public String testCreateProduct(@RequestBody Product newProd) {
 		
 		//Product p = new Product("fakeTitle", "fakeManufacturer", 99.99, "CategoryZ", "imageLinkHere");
@@ -37,6 +33,4 @@ public class AdminController {
 		return newProd.toString();
 		
 	}
-	
-
 }
